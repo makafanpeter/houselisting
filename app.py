91f235ec1483eda8c253d2d810c8f8240a704b7a
Peter Makafan <peter.makafan@venturegardengroup.com>
2016-07-07 14:40:40 +0100

    Changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, abort
 from flask.ext.sqlalchemy import SQLAlchemy
-import os
+from sqlalchemy import or_, and_
+import os, re
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -25,7 +26,25 @@ def search():
 
 @app.route('/update')
 def update():
-    print ("")
+    northEast = request.args.get('ne', '')
+    southWest = request.args.get('sw', '')
+    reg = re.compile(r'^-?\d+(?:\.\d+)?,-?\d+(?:\.\d+)?$')
+    if not (southWest and northEast):
+        abort(400)
+    if not (reg.match(southWest) and reg.match(northEast)):
+        abort(400)
+    northEastLat = northEast.split(',')[0]
+    northEastLong = northEast.split(',')[1]
+    southWestLat = southWest.split(',')[0]
+    southWestLong = southWest.split(',')[1]
+    houses = []
+    if southWestLong <= northEastLong:
+        houses = House.query.filter(and_(and_(House.latitude <= southWestLat, House.latitude <= northEastLat), and_(
+            House.longitude <= southWestLong, House.longitude <= northEastLong))).all()
+    else:
+        houses = House.query.filter(and_(and_(House.latitude <= southWestLat, House.latitude <= northEastLat), or_(
+            House.longitude <= southWestLong, House.longitude <= northEastLong))).all()
+    return jsonify(houses=[house.serialize for house in houses])
 
 
 if __name__ == '__main__':
--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,10 @@
  */
 
 
+
 var map;
+var markers = [];
+
 
 function initMap() {
 
@@ -29,11 +32,32 @@ function initMap() {
         scrollwheel: true
     };
 
-    map = new google.maps.Map(document.getElementById('map-canvas'), options);
+      // get DOM node in which map will be instantiated
+    var canvas = document.getElementById('map-canvas');
+
+    // instantiate map
+    map = new google.maps.Map(canvas, options);
+
+    // configure UI once Google Map is idle (i.e., loaded)
+    google.maps.event.addListenerOnce(map, "idle", configure);
 }
 
+var configure  = function () {
+      google.maps.event.addListener(map, "dragend", function () {
+        update();
+    });
 
-$("#search").typeahead({
+    // update UI after zoom level changes
+    google.maps.event.addListener(map, "zoom_changed", function () {
+        update();
+    });
+
+    // remove markers whilst dragging
+    google.maps.event.addListener(map, "dragstart", function () {
+        removeMarkers();
+    });
+
+    $("#search").typeahead({
         autoselect: true,
         highlight: true,
         minLength: 1
@@ -47,6 +71,24 @@ $("#search").typeahead({
     });
 
 
+    // re-enable ctrl- and right-clicking (and thus Inspect Element) on Google Map
+    // https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en
+    document.addEventListener("contextmenu", function (event) {
+        event.returnValue = true;
+        event.stopPropagation && event.stopPropagation();
+        event.cancelBubble && event.cancelBubble();
+    }, true);
+
+    // update UI
+    update();
+
+    // give focus to text box
+    $("#search").focus();
+};
+
+
+
+
 function search(query, callback) {
 
     var parameters = {
@@ -55,7 +97,7 @@ function search(query, callback) {
     $.getJSON("/search", parameters)
             .done(function (data, textStatus, jqXHR) {
                 var results = data.houses;
-                console.log(results)
+                console.log(results);
                 callback(results);
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
@@ -64,3 +106,58 @@ function search(query, callback) {
                 console.log(errorThrown.toString());
             });
 }
+
+
+var removeMarkers = function() {
+      for (var i = 0; i < markers.length; i++) {
+        if (markers[i]) {
+            markers[i].setMap(null);
+            markers[i] = null;
+        }
+    }
+}
+var addMarker = function (data) {
+    var newMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.latitude, data.longitude),
+        animation: google.maps.Animation.DROP,
+        map: map,
+    });
+
+      markers.push(newMarker);
+};
+/**
+ *
+ * Updates UI's markers.
+ */
+var  update = function()
+{
+    // get map's bounds
+    var bounds = map.getBounds();
+    var ne = bounds.getNorthEast();
+    var sw = bounds.getSouthWest();
+
+    // get places within bounds (asynchronously)
+    var parameters = {
+        ne: ne.lat() + "," + ne.lng(),
+        q: $("#search").val(),
+        sw: sw.lat() + "," + sw.lng()
+    };
+    $.getJSON("update", parameters)
+            .done(function (data, textStatus, jqXHR) {
+
+                // remove old markers from map
+                removeMarkers();
+                var houses = data.houses || [];
+                // add new markers to map
+                for (var i = 0; i < houses.length; i++)
+                {
+                    addMarker(houses[i]);
+                    console.log(houses[i]);
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+
+                // log error to browser's console
+                console.log(errorThrown.toString());
+            });
+};
